8fa3929338a582a5b17d48c030c4c1776089c27e
brogrammer232 <raymondmwaura232@gmail.com>
2025-07-09 23:15:57 +0300

    Modified to test the parser.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
-from pathlib import Path
 from scanner import get_human_readable_files
+from parser import get_links
+from pathlib import Path
 
-repo_root = Path("./repo_clone")
+# Test scanner.
+# repo_root = Path("./repo_clone")
+# files = get_human_readable_files(repo_root)
+# for file in files: print(file)
 
-files = get_human_readable_files(repo_root)
+# Test parser.
+test_file1 = Path("./repo_clone/SUMMARY.md")
+test_file2 = Path("./test.md")
+links = get_links(test_file1)
 
-for file in files: print(file)
+for link in links:
+    print(link)
