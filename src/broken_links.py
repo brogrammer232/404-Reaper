3a9b5db4b4e715faad0465b4cc3e48af999f8d7c
brogrammer232 <raymondmwaura232@gmail.com>
2025-08-13 09:29:47 +0300

    Added a function that returns a list of broken file links.

--- src/broken_links.py
+++ src/broken_links.py
@@ -9,25 +9,33 @@ This module takes a list of links and returns a list of broken links.
 """
 
 from parser import extract_links_from_file
-from typing import Union
 from pathlib import Path
 
-def get_broken_links(file: Union[str, Path]) -> list[str]:
-
-    if not isinstance(file, (str, Path)):
-        raise ValueError("Expected str or pathlib.Path for `file`.")
+def get_broken_links(file: str | Path) -> list[str]:
 
     file = Path(file)
 
     if not file.is_file():
-        raise FileNotFoundError(f"File does not exist or is a directory: {file}")
+        raise FileNotFoundError(f"File not found or is not a regular file: {file}")
 
-    # Extract links from the given markdown file.
     links = extract_links_from_file(file)
 
-def get_broken_file_links(file_links: list[tuple[str, str]]) -> list[tuple[str, str]]:
+    broken_file_links = get_broken_file_links(links["file links"], file.parent.resolve())
+
+    for link in broken_file_links: print(link)
+
+def get_broken_file_links(
+    file_links: list[tuple[str, str]],
+    base_dir: Path
+) -> list[tuple[str, str]]:
     """
-    Go through "file links" and scan for broken links.
-    Have a similar function for section links: `get_broken_section_links`.
+    Filter `file_links` to only include broken links.
     """
+
+    return [
+        link for link in file_links
+        if not (base_dir / link[-1]).resolve().exists()
+    ]
+
+def get_broken_section_links():
     pass
